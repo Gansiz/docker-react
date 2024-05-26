7b56235b31c0de2c7a576559b9966e2091ab15c5
Yurii Zhuha <yurii.zhuha@namecheap.com>
2024-05-26 04:37:21 +0300

    fixing pr

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Why?
+          Learn React now mf!
         </a>
       </header>
     </div>
